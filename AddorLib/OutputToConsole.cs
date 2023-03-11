2597e40452cacd8f3aa08ab63cbaa43c7a1bf4dd
Guoan-Wu <posttowganz@gmail.com>
2023-03-11 20:06:14 +1300

    Finished OutputToControl and its unit tests.

---
+++ AddorLib/OutputToConsole.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddorLib
+{
+    public class OutputToConsole : IOutput
+    {
+        private readonly string[] _header = {"| Previous value in file ","| User input value ",
+            "| Calculations ","| New value in file " };
+        private const string _border = "|";
+        private const char _filling = '-';
+
+        public void PrintHeader()
+        {
+            string l1 = "", l2 = "";
+            foreach (var s in _header)
+            {
+                l1 += s;
+                l2 += _border + new string(_filling, s.Length - 1);
+            }
+            l1 += _border;
+            l2 += _border;
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+        }
+        public void PrintNumber(IAddor data)
+        {
+            var fFormat = (string s, int index, bool last) =>
+            {
+                int len = _header[index].Length;
+                string format = "{0,-" + (len).ToString() + "}";
+                string s2 = string.Format(format, _border + " " + s);
+                if (last)
+                {
+                    s2 += _border;
+                    Console.WriteLine(s2);
+                }
+                else
+                {
+                    Console.Write(s2);
+                }
+            };
+
+            int index = 0;
+            string s = data.PrevValue?.ToString() ?? "";
+            fFormat(s, index, false); index++;
+
+            s = data.InputValue.ToString();
+            fFormat(s, index, false); index++;
+
+            s = data.Act ?? "";
+            fFormat(s, index, false); index++;
+
+            s = data.NewValue.ToString();
+            fFormat(s, index, true); index++;
+        }
+    }
+}
---
+++ UnitTest/OutputToConsoleTests.cs
@@ -0,0 +1,62 @@
+﻿using AddorLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [Collection("NonConcurrentTests")]
+    public class OutputToConsoleTests
+    {
+        [Fact]
+        public void TestPrintHeader()
+        {
+            // Arrange
+            string expectedValue = "| Previous value in file | User input value | Calculations | New value in file |\r\n";
+            expectedValue += "|------------------------|------------------|--------------|-------------------|\r\n";
+            IOutput output = new OutputToConsole();
+            //Act
+
+            //Assert
+            using (var sw = new StringWriter())
+            {
+                var current = Console.Out;
+                Console.SetOut(sw);
+                // Act
+                output.PrintHeader();
+
+                // Assert
+                string s = sw.ToString();
+                Assert.Equal(expectedValue, s);
+                Console.SetOut(current);
+            }
+        }
+        [Theory]
+        [InlineData(null, 3, "|                        | 3                | +3           | 3                 |\r\n")]
+        [InlineData(30, 122, "| 30                     | 122              | +122         | 152               |\r\n")]
+        [InlineData(132, 172, "| 132                    | 172              | +172, -152   | 152               |\r\n")]
+        public void TestPrintNumber(int? prevValue, int inputValue, string expectedValue)
+        {
+            // Arrange
+            IAddor addor = new AddorByMod();
+            int result = addor.Add(prevValue, inputValue);
+            IOutput output = new OutputToConsole();
+
+            using (var sw = new StringWriter())
+            {
+                var current = Console.Out;
+                Console.SetOut(sw);
+                // Act
+                output.PrintNumber(addor);
+                string s = sw.ToString();
+                Console.SetOut(current);
+
+                // Assert
+                Assert.Equal(expectedValue, s);
+            }
+        }
+    }
+}
+
